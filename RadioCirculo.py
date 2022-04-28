49ede4593cecb305862096b81a5346f04a936ff7
Mari Neri <mneri@tapterminal.com>
2022-04-28 11:51:30 -0500

    COMENTO CODIGO Y AGREGO LOS VALORES EN VARIABLES

--- RadioCirculo.py
+++ RadioCirculo.py
@@ -1,10 +1,24 @@
-print("RADIO DE UN CIRCULO")
+#saludo al inicio del programa
+print("RADIO DE UN CIRCULO\n")
+
 #importo la libreria math
 import math
+
+#creo las variables para el calculo
+a = 20
+b = 45
+c = 32
+
 #se crea una varible llamada semi donde hago una operacion matematica
-semi = (20 + 45 + 32) * (1/2)
+semi = (a + b + c) * (1/2)
+
+#Despues imprimo el semiperimetro usando el metodo str que devuelve el resultado en una cadena
 print('EL semiperimetro es: ' + str(semi))
 
-resultado = math.sqrt( semi * (semi-20) * (semi-45) * (semi-32) ) / semi
+#posterior a eso creo una variable llamada resultado
+# y utilizo la funcion matematica math con el metodo sqrt() que devuelve la raiz cuadrada de un numero
+resultado = math.sqrt( semi * (semi-a) * (semi-b) * (semi-c) ) / semi
+
+#y finalmente imprimo el resultado mostrando solo los ultimos 4 digitos de mi operacion
 print(f"EL resultado final es: {resultado:.4f}")
 
